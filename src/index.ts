1212cefe1d196366d935631845cc47330a3840d2
Kyle Edwards <edwards.kyle.a@gmail.com>
2021-07-26 08:09:55 -0500

    Clean up index exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Lexer } from './lexer';
 import { Parser } from './parser';
 import * as ast from './ast';
+import * as obj from './object';
+import { VM } from './vm';
 import { Token, TokenType, tokenIs } from './token';
 
-export { Lexer, Parser, ast, Token, TokenType, tokenIs };
+export { ast, obj, Lexer, Parser, Token, TokenType, tokenIs, VM };
